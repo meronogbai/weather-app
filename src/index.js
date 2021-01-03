45f56a74127a8c6c3f21daf0f63da537acb2e09f
Meron Ogbai <okbaymeron@gmail.com>
2021-01-03 14:42:55 +0300

    Add function to format data from api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ const apiKey = '3f4751a98d27d8f410e99e9589eaf508';
 async function getWeatherData(location) {
   const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
   const data = await fetch(endpoint);
-  const weather = await data.json();
-  console.log(weather.main.temp);
+  const weatherData = await data.json();
+  return weatherData;
 }
-getWeatherData('jeddah');
+
+async function formattedWeatherData(location) {
+  const data = await getWeatherData(location);
+  return {
+    temp: data.main.temp,
+    city: data.name,
+    country: data.sys.country,
+  };
+}
+formattedWeatherData('jeddah').then(res => console.log(res));
