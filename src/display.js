65deca49bf33cfeccc86d19505242997a0f896b3
Meron Ogbai <okbaymeron@gmail.com>
2021-01-03 20:43:37 +0300

    Fix style

--- src/display.js
+++ src/display.js
@@ -3,7 +3,7 @@ export function displayLoadingGif(content) {
     return null;
   }
   const loading = document.createElement('img');
-  loading.src = '../src/loading.gif';
+  loading.src = 'https://i.gifer.com/ZZ5H.gif';
   loading.id = 'loading';
   content.appendChild(loading);
   return true;
@@ -12,17 +12,20 @@ export function displayLoadingGif(content) {
 export function displayWeather(data, content) {
   const location = document.createElement('p');
   location.textContent = `${data.city}, ${data.country}`;
+  location.setAttribute('id', 'location');
   const temp = document.createElement('p');
+  temp.setAttribute('id', 'temp');
   temp.textContent = `${data.temp}K`;
-  const icon = document.createElement('div');
-  icon.innerHTML = `
-    <img src='http://openweathermap.org/img/wn/${data.weather.icon}@2x.png'>
-    <p>${data.weather.description}</p>
+  const weatherInfo = document.createElement('div');
+  weatherInfo.setAttribute('id', 'weatherInfo');
+  weatherInfo.innerHTML = `
+  <img src='http://openweathermap.org/img/wn/${data.weather.icon}@2x.png'>
+  <p>${data.weather.description}</p>
   `;
   content.innerHTML = '';
   content.appendChild(location);
   content.appendChild(temp);
-  content.appendChild(icon);
+  content.appendChild(weatherInfo);
 }
 
 export function displayError(content) {
