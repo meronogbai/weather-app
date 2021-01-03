80495c0dd953729b38c56c6665765dc4a3f99bb0
Meron Ogbai <okbaymeron@gmail.com>
2021-01-03 17:23:47 +0300

    Add function to display weather and city info

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ const apiKey = '3f4751a98d27d8f410e99e9589eaf508';
 
 const form = document.forms[0];
 
+function displayWeather(data) {
+  const content = document.getElementById('content');
+  content.innerHTML = '';
+  const location = document.createElement('p');
+  location.textContent = `${data.city}, ${data.country}`;
+  const temp = document.createElement('p');
+  temp.textContent = `${data.temp}K`;
+  content.appendChild(location);
+  content.appendChild(temp);
+}
 form.addEventListener('submit', async e => {
   e.preventDefault();
   const city = document.getElementById('city').value;
-  console.log(await formattedWeatherData(city, apiKey));
+  const data = await formattedWeatherData(city, apiKey);
+  displayWeather(data);
   form.reset();
 });
