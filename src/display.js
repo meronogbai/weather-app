48a4bf08411978427ff37887c70dc09f9bd16eb7
Meron Ogbai <okbaymeron@gmail.com>
2021-01-12 13:38:25 +0300

    Improve style of content and replace app screenshot

--- src/display.js
+++ src/display.js
@@ -4,18 +4,18 @@ const fahrenheitToCelsius = (degree) => Math.round((degree - 32) / 1.8);
 
 const addDegreeToggler = (content) => {
   const degreeToggle = document.createElement('button');
-  degreeToggle.classList = 'btn btn-success';
-  degreeToggle.textContent = 'Convert to Fahrenheit';
+  degreeToggle.classList = 'btn btn-dark';
+  degreeToggle.textContent = 'C to F';
   degreeToggle.addEventListener('click', () => {
     const temp = document.querySelector('#temp');
     const number = Number(temp.textContent.slice(0, -2));
     const unit = temp.textContent.slice(-1);
     if (unit === 'C') {
       temp.textContent = `${celsiustoFahrenheit(number)}°F`;
-      degreeToggle.textContent = 'Convert to Celsius';
+      degreeToggle.textContent = 'F to C';
     } else {
       temp.textContent = `${fahrenheitToCelsius(number)}°C`;
-      degreeToggle.textContent = 'Convert to Fahrenheit';
+      degreeToggle.textContent = 'C to F';
     }
   });
   content.appendChild(degreeToggle);
@@ -45,23 +45,18 @@ export const displayLoadingGif = (content) => {
 };
 
 export const displayWeather = (data, content) => {
-  const location = document.createElement('p');
-  location.textContent = `${data.city}, ${data.country}`;
-  location.setAttribute('id', 'location');
-  const temp = document.createElement('p');
-  temp.setAttribute('id', 'temp');
-  temp.textContent = `${data.temp}°C`;
-  const weatherInfo = document.createElement('div');
-  weatherInfo.setAttribute('id', 'weatherInfo');
-  weatherInfo.innerHTML = `
-  <img src='https://openweathermap.org/img/wn/${data.weather.icon}@2x.png'>
-  <p>${data.weather.description}</p>
-  `;
-  content.innerHTML = '';
+  content.classList = 'card small-card mx-auto';
+  content.innerHTML = `
+    <div class='card-header'>
+      <img src='https://openweathermap.org/img/wn/${data.weather.icon}@2x.png' width='40'>
+      <span class='card-text' id='weatherInfo'>${data.weather.description}</span>
+    </div>
+    <div class='card-body'>
+      <h5 class='card-title' id='location'>${data.city}, ${data.country}</h5>
+      <p class='card-text'>Temperature: <span id='temp'>${data.temp}°C</span></p>
+    </div>
+    `;
   setBackground(data.weather.icon);
-  content.appendChild(location);
-  content.appendChild(temp);
-  content.appendChild(weatherInfo);
   addDegreeToggler(content);
 };
 
